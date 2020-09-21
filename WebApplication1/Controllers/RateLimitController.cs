18c87b812c7b0433a674643d197b35cd04d6dc57
Dino <dino.lai@titansoft.com.sg>
2020-09-21 23:20:29 +0800

    commit the project all

---
+++ WebApplication1/Controllers/RateLimitController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RateLimitController : ControllerBase
+    {
+        private static DateTime previousTimePoint = DateTime.Now;
+        private static Hashtable IpCountHt = new Hashtable();
+
+        private readonly ILogger<VotingController> _logger;
+
+        public RateLimitController(ILogger<VotingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            DateTime requestTime = DateTime.Now;
+            TimeSpan ts = requestTime - previousTimePoint;
+
+            if(ts.TotalSeconds > 60)
+            {
+                previousTimePoint = requestTime;
+                IpCountHt.Clear();
+            }
+            
+
+            var requestIp = Request.HttpContext.Connection.RemoteIpAddress;
+            if(IpCountHt.Contains(requestIp))
+            {
+                var count = IpCountHt[requestIp] as int?;
+                IpCountHt[requestIp] = ++count;
+            }
+            else
+            {
+                IpCountHt.Add(requestIp, 1);
+            }
+
+
+            if((int)IpCountHt[requestIp] >60)
+            {
+                return $"maximum request limit reached, please hold for {(previousTimePoint.AddMinutes(1) - requestTime):ss} seconds";
+            }
+            return $"request received, {IpCountHt[requestIp]}/60 since {previousTimePoint}";
+        }
+    }
+}
---
+++ WebApplication1/Controllers/VotingController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VotingController : ControllerBase
+    {
+        private static int agree;
+        private static int disagree;
+
+        private readonly ILogger<VotingController> _logger;
+
+        public VotingController(ILogger<VotingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return $"is Wuhan Coronavirus created from China? Agree:{agree}, Disagree:{disagree}";
+        }
+
+        [HttpPost]
+        public string Vote([FromForm] bool isAgree)
+        {
+
+            if (isAgree)
+                agree++;
+            else
+                disagree++;
+
+            return  $"ErrorCode : 0, voting: {isAgree}";
+        }
+    }
+}
